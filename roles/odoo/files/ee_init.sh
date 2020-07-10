fd53109e5e8771486097fa7621e01532724a7d94
chendelin1982 <chendelin1982@126.com>
2020-07-10 19:00:42 +0800

    Update ee_init.sh

--- roles/odoo/files/ee_init.sh
+++ roles/odoo/files/ee_init.sh
@@ -3,5 +3,6 @@
 read -p "Input your test key:" domain
 rm -rf /data/wwwroot/odoo.html
 rm -rf /etc/nginx/conf.d/default.conf
-mv /etc/nginx/conf.d/default*[@]* /etc/nginx/conf.d/default.conf
+mv /etc/nginx/conf.d/default*[@]* /etc/nginx/conf.d/default.conf &>> /dev/null
 systemctl restart nginx
+echo "Unlock Odoo enterprise successfully, please refresh and clear cach of your browser to visit Odoo again"
