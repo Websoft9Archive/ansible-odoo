dc1030c5cf696a18aa7ae8b6cc63fd24fa7200cf
chendelin1982 <chendelin1982@126.com>
2020-07-10 18:47:09 +0800

    Update ee_init.sh

--- roles/odoo/files/ee_init.sh
+++ roles/odoo/files/ee_init.sh
@@ -3,5 +3,5 @@
 read -p "Input your test key:" domain
 rm -rf /data/wwwroot/odoo.html
 rm -rf /etc/nginx/conf.d/default.conf
-mv /etc/nginx/conf.d/default.conf* /etc/nginx/conf.d/default.conf
+mv /etc/nginx/conf.d/default*[@]* /etc/nginx/conf.d/default.conf
 systemctl restart nginx
