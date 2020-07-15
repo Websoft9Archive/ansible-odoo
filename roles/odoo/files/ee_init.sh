a023e92f6f1e40926538dc83a87eb3c65c3efb79
chendelin1982 <chendelin1982@126.com>
2020-07-15 16:21:50 +0800

    Update ee_init.sh

--- roles/odoo/files/ee_init.sh
+++ roles/odoo/files/ee_init.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-read -p "是否已经联系Odoo获取试用秘钥[ y/n ]:" getkkey
+read -p "已获得试用秘钥？[ y/n ]:" getkkey
 if [ $getkkey = "n" ]; then echo -e "\n请通过浏览器访问 http://服务器公网IP 获取秘钥"&&exit; fi
-rm -rf /data/wwwroot/odoo.html
-rm -rf /etc/nginx/conf.d/default.conf
-mv /etc/nginx/conf.d/default*[@]* /etc/nginx/conf.d/default.conf &>> /dev/null
-systemctl restart nginx
-echo -e "\n解锁成功！请清空浏览器[缓存]后，重新访问 Odoo"
+
+password_md5=ce51fc1c9a0d4ff09bb512b403daff98
+while true;
+do
+  read -p "请输入试用秘钥:" password
+  password_do_md5=$(echo -n $password | md5sum | cut -d " " -f 1)
+  if [ $password_md5 == $password_do_md5 ];then
+          rm -rf /data/wwwroot/odoo.html
+          rm -rf /etc/nginx/conf.d/default.conf
+          mv /etc/nginx/conf.d/default*[@]* /etc/nginx/conf.d/default.conf &>> /dev/null
+          systemctl restart nginx
+          echo -e "\033[32m \n解锁成功！请清空浏览器[缓存]后，重新访问 Odoo \033[0m"
+          break
+  fi
+echo -e "\033[31m 秘钥错误！请重新输入\033[0m"
+done
